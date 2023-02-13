11e68a810c98ddb70c9e20097c3c56db5be1a2d6
Cristhian Bitencourt <cristhianthiagok@yahoo.com>
2023-02-13 11:58:18 -0300

    while part 2

---
+++ aula38.py
@@ -0,0 +1,13 @@
+qnt_colunas = 5
+qnt_linhas = 5
+
+linhas = 1
+
+while linhas <= qnt_linhas:
+    colunas = 1
+
+    while colunas <= qnt_colunas:
+        print(f'{linhas=}, {colunas=}')
+        colunas += 1
+
+    linhas += 1
